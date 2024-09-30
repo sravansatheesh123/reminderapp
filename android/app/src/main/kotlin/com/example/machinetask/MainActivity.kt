4afd959c002c6de0989d32f4e90b25c12e2f0246
sravansatheesh123 <sravandpz2528@gmail.com>
2024-09-30 11:16:16 +0530

    datepickern

---
+++ android/app/src/main/kotlin/com/example/machinetask/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.machinetask
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(ReminderApp());
+}
+
+class ReminderApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Reminder App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ReminderHomePage(),
+    );
+  }
+}
+
+class ReminderHomePage extends StatefulWidget {
+  @override
+  _ReminderHomePageState createState() => _ReminderHomePageState();
+}
+
+class _ReminderHomePageState extends State<ReminderHomePage> {
+  DateTime? _selectedDate;
+  TimeOfDay? _selectedTime;
+  final _reminderController = TextEditingController();
+
+  Future<void> _pickDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2021),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != _selectedDate)
+      setState(() {
+        _selectedDate = picked;
+      });
+  }
+
+
+    );
+  }
+}
